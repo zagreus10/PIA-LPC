057dab057a954b274cb4d257ab1db6d2de381550
zagreus10 <142361285+zagreus10@users.noreply.github.com>
2023-11-21 18:30:43 -0600

    Update superscan.sh

--- 02 - Scripting en Bash/superscan.sh
+++ 02 - Scripting en Bash/superscan.sh
@@ -13,11 +13,11 @@ echo "||"
 read -p "Opción  [ 1 - 4 ] " c
 
 case $c in
-    1) $HOME/netdiscover.sh;;
+    1) $HOME/netdiscover.sh;; #Cambiar por la ruta
     2) echo "Ejecutando portscanv1.sh"
-       $HOME/portscanv1.sh;;
+       $HOME/portscanv1.sh;; #Cambiar por la ruta
     3) echo "Ejecutando welcome.sh"
-       $HOME/welcome.sh;;
+       $HOME/welcome.sh;; #Cambiar por la ruta
     4) echo "Bye!"; exit 0;;
     *) echo "Opción no válida. Introduce un número del 1 al 4.";;
 esac
